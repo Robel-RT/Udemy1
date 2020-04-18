a3d0e7abc3f380abf8b9f1207e7349eacd454b03
robelteklay92 <robielteklay92@gmail.com>
2020-04-18 09:54:05 +0100

    list applications

---
+++ dataopertion.py
@@ -0,0 +1,14 @@
+monday_temprature = [9.1, 8.8, 7.5]
+monday_temprature.append(6.3)
+print(monday_temprature)
+
+monday_temprature = [9.1, 8.8, 7.5, 6.3, 8.2]
+monday_temprature[2:]
+print(monday_temprature)
+
+monday_temprature[-3]
+print(monday_temprature)
+
+mystring = ['hello', 4, 5.6, 9]
+print(mystring[0][2])
+
